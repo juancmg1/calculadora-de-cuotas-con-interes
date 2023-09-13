bef1ad62ab0473d19a8bdad9fb7518079347aaaf
juancmg <juancmg002@gmail.com>
2023-09-12 22:52:26 -0300

    version1

---
+++ main.js
@@ -0,0 +1,87 @@
+
+
+let valor = parseFloat(prompt("Ingresa el valor"));
+let tarjeta = prompt("¿Tu tarjeta es Visa, Master o Amex?");
+let cuotas = parseInt(prompt("Cantidad de cuotas"));
+
+switch (tarjeta) {
+    case "visa":
+        switch (cuotas) {
+            case 3:
+                let resultadoVisa3 =(((11.5 / 3) / 100) * valor + valor);
+                alert("el total es: " + resultado3)
+                let valorPorCuotaVisa3 = resultadoVisa3 / 3;
+                alert("Te quedarían " + valorPorCuotaVisa3 + " por cuota");
+            break;
+            case 6:
+                let resultadoVisa6 =(((24.5 / 6) / 100) * valor + valor);
+                alert("el total es: " + resultadoVisa6)
+                let valorPorCuotaVisa6 = resultadoVisa6 / 6;
+                alert("Te quedarían " + valorPorCuotaVisa6 + " por cuota");
+             
+            break;
+            case 12:
+                let resultadoVisa12 =(((53.5 / 12) / 100) * valor + valor);
+                alert("el total es: " + resultadoVisa12)
+                let valorPorCuotaVisa12 = resultadoVisa12 / 12;
+                alert("Te quedarían " + valorPorCuotaVisa12 + " por cuota");
+            break;
+             default:
+                alert("Número de cuotas no válido para Visa");
+                break;
+        }
+        break;
+        case "master":
+        switch (cuotas) {
+            case 3:
+                let resultadoMaster3 =(((12.5 / 3) / 100) * valor + valor);
+                alert("el total es: " + resultadoMaster3)
+                let valorPorCuotaMaster3 = resultadoMaster3 / 3;
+                alert("Te quedarían " + valorPorCuotaMaster3 + " por cuota");
+            break;
+            case 6:
+                let resultadoMaster6 =(((21.5 / 6) / 100) * valor + valor);
+                alert("el total es: " + resultadoMaster6)
+                let valorPorCuotaMaster6 = resultadoMaster6 / 6;
+                alert("Te quedarían " + valorPorCuotaMaster6 + " por cuota");
+            break;
+            case 12:
+                let resultadoMaster12 =(((58 / 12) / 100) * valor + valor);
+                alert("el total es: " + resultadoMaster12)
+                let valorPorCuotaMaster12 = resultadoMaster12 / 12;
+                alert("Te quedarían " + valorPorCuotaMaster12 + " por cuota");
+            break;
+             default:
+                alert("Número de cuotas no válido para master");
+                break;
+        }
+        break;
+        case "amex":
+        switch (cuotas) {
+            case 3:
+                let resultadoAmex3 =(((12.5 / 3) / 100) * valor + valor);
+                alert("el total es: " + resultadoAmex3)
+                let valorPorCuotaAmex3 = resultadoAmex3 / 3;
+                alert("Te quedarían " + alorPorCuotaAmex3 + " por cuota");
+            break;
+            case 6:
+                let resultadoAmex6 =(((25.5 / 6) / 100) * valor + valor);
+                alert("el total es: " + resultadoAmex6)
+                let valorPorCuotaAmex6 = resultadoAmex6 / 6;
+                alert("Te quedarían " + valorPorCuotaAmex6 + " por cuota");
+            break;
+            case 12:
+                let resultadoAmex12 =(((12.5 / 3) / 100) * valor + valor);
+                alert("el total es: " + resultadoAmex12)
+                let valorPorCuotaAmex12 = resultadoAmex12 / 12;
+                alert("Te quedarían " + valorPorCuotaAmex12 + " por cuota");
+            break;
+             default:
+                alert("Número de cuotas no válido para amex");
+                break;
+        }
+    }
+
+
+
+
