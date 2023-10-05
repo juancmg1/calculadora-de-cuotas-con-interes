1c1aeb6166d34c5243d133b41b0b1c10f889be48
juancmg <juancmg002@gmail.com>
2023-10-04 22:36:23 -0300

    vercion 3

--- main.js
+++ main.js
@@ -1,120 +1,132 @@
+alert("Bienvenido a Jshop");
+let tipodeproducto = prompt("¿Qué quieres comprar: una TV, celular o notebook?");
+
+const arraytv = [
+  { id: 0, producto: "tv55", precio: 200000 },
+  { id: 1, producto: "tv65", precio: 340000 },
+  { id: 2, producto: "tv75", precio: 500000 }
+];
+
+const arraycelulares = [
+  { id: 0, producto: "iphone14", precio: 500000 },
+  { id: 1, producto: "iphone14promax", precio: 800000 },
+  { id: 2, producto: "iphone15promax", precio: 1000000 }
+];
+
+const arraynotebooks = [
+  { id: 0, producto: "book3", precio: 500000 },
+  { id: 1, producto: "boook3pro", precio: 800000 },
+  { id: 2, producto: "book3pro360", precio: 1200000 }
+];
+
+function elecciondeproducto(array) {
+  const stock = array.map(producto => `${producto.producto} - Precio: $${producto.precio}`).join('\n');
+  let productoelegido = prompt(`¿Qué opción deseas (1, 2 o 3)?\n${stock}`);
+  
+  productoelegido = parseInt(productoelegido);
+
+  if (!isNaN(productoelegido) && productoelegido >= 1 && productoelegido <= 3) {
+    const eleccion = array.find(producto => producto.id === productoelegido - 1);
+    if (eleccion) {
+      const valor = eleccion.precio;
+      alert("Elegiste: " + eleccion.producto + " - $" + valor);
+      return valor; 
+    } else {
+      alert("Opción no válida");
+      return null; 
+    }
+  } else {
+    alert("Opción no válida");
+    return null; 
+  }
+}
+
+const valorProducto = elecciondeproducto(arraytv);
 
 
-let valor;
-while (true) {
-  let input = prompt("Ingresa el valor");
-  if (input !== null && input.trim() !== "") {
-    valor = parseFloat(input);
-    if (!isNaN(valor)) {
+if (valorProducto !== null) {
+  let tarjeta;
+  while (true) {
+    tarjeta = prompt("¿Tu tarjeta es Visa, Master o Amex?");
+    if (tarjeta !== null && tarjeta.trim() !== "") {
       break;
     }
+    alert("Debes seleccionar una tarjeta.");
   }
-  alert("Debes ingresar un valor numérico válido.");
-}
 
-let tarjeta;
-while (true) {
-  tarjeta = prompt("¿Tu tarjeta es Visa, Master o Amex?");
-  if (tarjeta !== null && tarjeta.trim() !== "") {
-    // Puedes agregar más validaciones aquí si es necesario
-    break;
-  }
-  alert("Debes seleccionar una tarjeta.");
-}
-
-let cuotas;
-while (true) {
-  let input = prompt("Cantidad de cuotas");
-  if (input !== null && input.trim() !== "") {
-    cuotas = parseInt(input);
-    if (!isNaN(cuotas)) {
-      break;
+  let cuotas;
+  while (true) {
+    let input = prompt("Cantidad de cuotas");
+    if (input !== null && input.trim() !== "") {
+      cuotas = parseInt(input);
+      if (!isNaN(cuotas)) {
+        break;
+      }
     }
+    alert("Debes ingresar un número de cuotas válido.");
   }
-  alert("Debes ingresar un número de cuotas válido.");
-}
-
-// Ahora puedes usar las variables valor, tarjeta y cuotas en tu código
 
 
-switch (tarjeta) {
+  function calcularTotal(interes) {
+    let total = ((interes / 12) / 100) * valorProducto + valorProducto;
+    alert("El total es: $" + total);
+    let valorPorCuota = total / cuotas;
+    alert(`Te quedarían ${cuotas} cuotas de $${valorPorCuota.toFixed(2)} cada una`);
+  }
+
+  switch (tarjeta.toLowerCase()) {
     case "visa":
-        switch (cuotas) {
-            case 3:
-                let resultadoVisa3 =(((11.5 / 3) / 100) * valor + valor);
-                alert("el total es: " + resultado3)
-                let valorPorCuotaVisa3 = resultadoVisa3 / 3;
-                alert("Te quedarían " + valorPorCuotaVisa3 + " por cuota");
-            break;
-            case 6:
-                let resultadoVisa6 =(((24.5 / 6) / 100) * valor + valor);
-                alert("el total es: " + resultadoVisa6)
-                let valorPorCuotaVisa6 = resultadoVisa6 / 6;
-                alert("Te quedarían " + valorPorCuotaVisa6 + " por cuota");
-             
-            break;
-            case 12:
-                let resultadoVisa12 =(((53.5 / 12) / 100) * valor + valor);
-                alert("el total es: " + resultadoVisa12)
-                let valorPorCuotaVisa12 = resultadoVisa12 / 12;
-                alert("Te quedarían " + valorPorCuotaVisa12 + " por cuota");
-            break;
-             default:
-                alert("Número de cuotas no válido para Visa");
-                break;
-        }
-        break;
-        case "master":
-        switch (cuotas) {
-            case 3:
-                let resultadoMaster3 =(((12.5 / 3) / 100) * valor + valor);
-                alert("el total es: " + resultadoMaster3)
-                let valorPorCuotaMaster3 = resultadoMaster3 / 3;
-                alert("Te quedarían " + valorPorCuotaMaster3 + " por cuota");
-            break;
-            case 6:
-                let resultadoMaster6 =(((21.5 / 6) / 100) * valor + valor);
-                alert("el total es: " + resultadoMaster6)
-                let valorPorCuotaMaster6 = resultadoMaster6 / 6;
-                alert("Te quedarían " + valorPorCuotaMaster6 + " por cuota");
-            break;
-            case 12:
-                let resultadoMaster12 =(((58 / 12) / 100) * valor + valor);
-                alert("el total es: " + resultadoMaster12)
-                let valorPorCuotaMaster12 = resultadoMaster12 / 12;
-                alert("Te quedarían " + valorPorCuotaMaster12 + " por cuota");
-            break;
-             default:
-                alert("Número de cuotas no válido para master");
-                break;
-        }
-        break;
-        case "amex":
-        switch (cuotas) {
-            case 3:
-                let resultadoAmex3 =(((12.5 / 3) / 100) * valor + valor);
-                alert("el total es: " + resultadoAmex3)
-                let valorPorCuotaAmex3 = resultadoAmex3 / 3;
-                alert("Te quedarían " + alorPorCuotaAmex3 + " por cuota");
-            break;
-            case 6:
-                let resultadoAmex6 =(((25.5 / 6) / 100) * valor + valor);
-                alert("el total es: " + resultadoAmex6)
-                let valorPorCuotaAmex6 = resultadoAmex6 / 6;
-                alert("Te quedarían " + valorPorCuotaAmex6 + " por cuota");
-            break;
-            case 12:
-                let resultadoAmex12 =(((12.5 / 3) / 100) * valor + valor);
-                alert("el total es: " + resultadoAmex12)
-                let valorPorCuotaAmex12 = resultadoAmex12 / 12;
-                alert("Te quedarían " + valorPorCuotaAmex12 + " por cuota");
-            break;
-             default:
-                alert("Número de cuotas no válido para amex");
-                break;
-        }
-    }
-
-
+      switch (cuotas) {
+        case 3:
+          calcularTotal(11.4);
+          break;
+        case 6:
+          calcularTotal(24.4);
+          break;
+        case 12:
+          calcularTotal(52.5);
+          break;
+        default:
+          alert("Número de cuotas no válido para Visa");
+          break;
+      }
+      break;
+    case "master":
+      switch (cuotas) {
+        case 3:
+          calcularTotal(12.3);
+          break;
+        case 6:
+          calcularTotal(27.4);
+          break;
+        case 12:
+          calcularTotal(55.5);
+          break;
+        default:
+          alert("Número de cuotas no válido para Master");
+          break;
+      }
+      break;
+    case "amex":
+      switch (cuotas) {
+        case 3:
+          calcularTotal(10.3);
+          break;
+        case 6:
+          calcularTotal(31.4);
+          break;
+        case 12:
+          calcularTotal(51.5);
+          break;
+        default:
+          alert("Número de cuotas no válido para Amex");
+          break;
+      }
+      break;
+    default:
+      alert("Tarjeta no reconocida");
+      break;
+  }
+}
 
 
